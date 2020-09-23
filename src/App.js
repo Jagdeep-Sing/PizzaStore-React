ef0789d7a9aec232d7fbceed3aa7002d45a05877
jagdeep-sing <js86248@gmail.com>
2020-09-23 17:40:15 +0530

    Add google fonts

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
 
   body {
-    background-color: blue;
+    font-family: 'Open Sans', sans-serif;
+    h1, h2, h3{
+      font-family: 'Righteous', cursive;
+    }
   }
 
 `
 function App() {
   return (
     <>
+    <h1>Pizza Store</h1>
       <GlobalStyle />
       <div>Welcome to pizza store</div>
    </>
