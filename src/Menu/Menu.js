7884b3df92f62a21eda4fb32b40ffa5686a93aff
jagdeep-sing <js86248@gmail.com>
2020-09-24 16:50:36 +0530

    food sections

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -8,8 +8,11 @@ const MenuStyled = styled.div`
     height: 1000px;
 `
 export function Menu() {
-    return <MenuStyled> 
-                <h1>Menu</h1>
+    return (
+    <MenuStyled> 
+        {Object.entries(foods).map(([sectionName, foods]) => (
+            <>
+                <h1>{sectionName}</h1>
                 <FoodGrid>
                     {foods.map(food => (
                         <Food img={food.img}>
@@ -19,6 +22,9 @@ export function Menu() {
                         </Food>
                     ))}
                 </FoodGrid>
+            </>
+        ))}
+                
             
-            </MenuStyled>
+    </MenuStyled>);
 }
