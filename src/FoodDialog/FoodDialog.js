a15ac95771c4ea62b58560dc0899aa8e23843edb
jagdeep-sing <js86248@gmail.com>
2020-09-25 11:05:18 +0530

    add dialog and order footer

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FoodLabel } from "../Menu/FoodGrid";
-
+import {pizzaRed} from '../Styles/colors';
+import { Title } from "../Styles/title";
 const Dialog = styled.div`
     width: 500px;
-    height: 500px;
     background-color: white;
     position: fixed;
     top: 75px;
@@ -19,7 +19,31 @@ const DialogBanner = styled.div`
     ${({img}) => `background-image: url(${img});`};
     background-position: center;
     background-size: cover;
-`
+`;
+export const DialogContent = styled.div`
+    overflow: auto;
+    min-height: 100px;
+`;
+
+export const DialogFooter = styled.div`
+    box-shadow: 0px -2px 10px 0px grey;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+`;
+
+export const ConfirmButton = styled(Title)`
+    margin: 10px;
+    color: white;
+    height: 20px;
+    border-radius: 5px;
+    padding: 10px;
+    text-align: center;
+    width: 200px;
+    cursor: pointer;
+    background-color: ${pizzaRed};
+`;
+
 const DialogShadow = styled.div`
     position: fixed;
     height: 100%;
@@ -34,7 +58,7 @@ const DialogBannerName = styled(FoodLabel)`
     top: 100px;
     font-size: 30px;
     padding: 5px 40px;
-`
+`;
 
 export function FoodDialog({openFood, setOpenFood}) {
     function close() {
@@ -48,6 +72,12 @@ export function FoodDialog({openFood, setOpenFood}) {
                     <DialogBanner img={openFood.img}>
                         <DialogBannerName>{openFood.name}</DialogBannerName>
                     </DialogBanner>
+                    <DialogContent />
+                    <DialogFooter>
+                        <ConfirmButton>
+                            Add to order
+                        </ConfirmButton>
+                    </DialogFooter>
                 </Dialog>
             </>) : null
         );
